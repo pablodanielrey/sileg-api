6bed46c3758d2838a1997be73aea5f84fda6e13d
Pablo Daniel Rey <pablo@econo.unlp.edu.ar>
2018-11-22 14:43:05 -0300

    me faltaba eliminar de la importacion

--- src/sileg/model/entities/__init__.py
+++ src/sileg/model/entities/__init__.py
@@ -1,6 +1,6 @@
 
 from .Lugar import Catedra, Departamento, Lugar, LugarDictado, Secretaria, Instituto, Prosecretaria, Escuela, Maestria, Direccion, Centro
-from .Cargo import Cargo, CumpleFunciones, Docente, NoDocente
+from .Cargo import Cargo
 from .Designacion import Designacion, Categoria
 from .Materia import Materia
 
@@ -19,8 +19,5 @@ __all__ = [
     'Instituto',
     'Prosecretaria',
     'Maestria',
-    'Cargo',
-    'Docente',
-    'NoDocente',
-    'CumpleFunciones'
+    'Cargo'
 ]
