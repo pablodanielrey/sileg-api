491878d3b0723401c3e16fc6d4c26687416e4319
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-11-24 10:07:11 -0300

    elimine el import

--- src/sileg/model/entities/__init__.py
+++ src/sileg/model/entities/__init__.py
@@ -1,12 +1,11 @@
 
 from .Lugar import Catedra, Departamento, Lugar, LugarDictado, Secretaria, Instituto, Prosecretaria, Escuela, Maestria, Direccion, Centro
 from .Cargo import Cargo
-from .Designacion import Designacion, Categoria
+from .Designacion import Designacion
 from .Materia import Materia
 
 __all__ = [
     'Catedra',
-    'Categoria',
     'Centro',
     'Departamento',
     'Designacion',
