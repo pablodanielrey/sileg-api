ac6ab29d908d01c9404b95fddfa87841e7a12e91
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-12-06 19:38:16 -0300

    cambie el puerto para que sea coherente con el de wsgi

--- src/sileg/api/rest/main.py
+++ src/sileg/api/rest/main.py
@@ -384,7 +384,7 @@ def add_header(r):
     return r
 
 def main():
-    app.run(host='0.0.0.0', port=9005, debug=False)
+    app.run(host='0.0.0.0', port=10202, debug=False)
 
 if __name__ == '__main__':
     main()
