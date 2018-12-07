d28900d857703afe8ca70a1200718c6681dc5c93
Emanuel Pais <emanuel84@gmail.com>
2018-12-07 19:17:59 -0300

    Quedo funcionando la importacion de designaciones, con sus prorrogas y sus bajas. Falta las extensiones

--- scripts/sincronizar_desde_sileg.py
+++ scripts/sincronizar_desde_sileg.py
@@ -10,25 +10,31 @@ from sileg.model import obtener_session
 
 tipo_designacion = ['original', 'baja', 'prorroga', 'extension', 'prorroga de extension']
 
-def crear_designacion(desde=None, hasta=None, old_id=None, historico=False, tipo=tipo_designacion[0], 
-                    designacion_relacionada=None, resolucion_id=None, observaciones=''):
-    d = Designacion()
-    d.id = str(uuid.uuid4())
+def crear_designacion(session=None,desde=None, hasta=None, old_id=None, historico=False, tipo=tipo_designacion[0], 
+                    designacion_relacionada=None, resolucion_id=None, observaciones='', categoria=None):
+    d = session.query(Designacion).filter(Designacion.old_id == old_id).one_or_none()
+    if d is None:
+        d = Designacion()        
+        d.old_id = old_id
+        
     d.desde = desde
     d.hasta = hasta
     d.historico = historico
     d.tipo = tipo
-    d.designacion_id = designacion_relacionada    
-    d.old_id = old_id
-    logging.info(d.__dict__)
+    d.designacion_id = designacion_relacionada
+    d.categorias = [categoria] if categoria else []
+    if not d.id:
+        d.id = str(uuid.uuid4())
+        session.add(d)
     return d
 
 def obtener_prorroga(prorroga_id, cur):
+    logging.info("Obtener prorroga id: {}".format(prorroga_id))
     cur.execute("""
-        SELECT prorroga_id, prorroga_fecha_desde, prorroga_fecha_hasta, prorroga_prorrogada_con
+        SELECT *
         FROM prorroga
         WHERE prorroga_id = %s
-        ORDER BY 
+        ORDER BY prorroga_fecha_desde
     """,(prorroga_id,))
     return cur.fetchone()
 
@@ -47,30 +53,48 @@ def obtener_extensiones(desig_id, cur):
     cur.execute(" SELECT * FROM extension WHERE extension_designacion_id = %s",(desig_id,))
     return cur.fetchall()
 
-def importar_designacion(d, cur):
+def importar_designacion(d, cur, session):
     logging.info("Importando designacion: {}".format(d))
     id = d["desig_id"]
-
-
-    desig = crear_designacion(desde=d["desig_fecha_desde"], hasta=d["desig_fecha_hasta"], old_id=id, historico=False, tipo=tipo_designacion[0], 
+    old_id = "desig_{}".format(id)
+    desig = crear_designacion(session=session, desde=d["desig_fecha_desde"], hasta=d["desig_fecha_hasta"], old_id=old_id, historico=False, tipo=tipo_designacion[0], 
                             designacion_relacionada=None, resolucion_id=d["desig_resolucionalta_id"], observaciones=d["desig_observaciones"])
-    
-    if d["desig_tipobaja_id"]:
+
+    logging.info("Desginacion creada: {}".format(desig.__dict__))    
+    session.commit()
+    if d["desig_resolucionbaja_id"]:
+        logging.info("Crear baja: {}".format(d))
+        old_id = "baja_{}".format(id)
         desde = d["desig_fecha_baja"] if d["desig_fecha_baja"] else d["desig_fecha_hasta"]
-        crear_designacion(desde=desde, hasta=None, old_id=id, historico=False, tipo=tipo_designacion[1],
-                         designacion_relacionada=desig.id, resolucion_id=d["desig_resolucionbaja_id"], observaciones=d["desig_observaciones"])
+        categoria = None 
+        if d["desig_tipobaja_id"]:
+            categoria = session.query(CategoriaDesignacion).filter(CategoriaDesignacion.old_id == str(d["desig_tipobaja_id"])).one_or_none()
+        crear_designacion(session=session, desde=desde, hasta=None, old_id=old_id, historico=True, tipo=tipo_designacion[1], designacion_relacionada=desig.id, 
+                         resolucion_id=d["desig_resolucionbaja_id"], observaciones=d["desig_observaciones"], categoria=categoria)
         desig.historico = True
 
     prorrogas = []
     if d["desig_prorrogada_con"]:
         prorrogas = obtener_prorrogas_de(d["desig_prorrogada_con"], cur)
         for p in prorrogas:
-            historico = p["prorroga_prorrogada_con"] is None or p["prorroga_prorrogada_con"] == ""
-            crear_designacion(desde=p["prorroga_fecha_desde"], hasta=p["prorroga_fecha_hasta"], old_id=p["prorroga_id"], historico=historico, tipo=tipo_designacion[2],
-                              designacion_relacionada=desig.id, resolucion_id=d["desig_resolucionbaja_id"], observaciones=d["desig_observaciones"])            
+            historico = p["prorroga_prorrogada_con"] is None or p["prorroga_resolucionbaja_id"] is not None
+            old_id = "prorroga_{}".format(p["prorroga_id"])
+            pro_desig = crear_designacion(session=session,desde=p["prorroga_fecha_desde"], hasta=p["prorroga_fecha_hasta"], old_id=old_id, historico=historico, tipo=tipo_designacion[2],
+                              designacion_relacionada=desig.id, resolucion_id=d["desig_resolucionalta_id"], observaciones=d["desig_observaciones"])            
             logging.info("Crear prorroga: {}".format(p))
+            session.commit()
+            # verifico si tiene una baja
+            if p["prorroga_resolucionbaja_id"]:
+                old_id = "baja_{}".format(p["prorroga_id"])
+                # si tiene baja creo la designacion baja y la relaciono con la prorroga
+                desde = p["prorroga_fecha_baja"] if p["prorroga_fecha_baja"] else p["prorroga_fecha_hasta"]
+                categoria = None 
+                if p["prorroga_tipobaja_id"]:
+                    categoria = session.query(CategoriaDesignacion).filter(CategoriaDesignacion.old_id == str(p["prorroga_tipobaja_id"])).one_or_none()
+                crear_designacion(session=session, desde=desde, hasta=None, old_id=old_id, historico=False, tipo=tipo_designacion[1],
+                         designacion_relacionada=desig.id, resolucion_id=d["desig_resolucionbaja_id"], observaciones=d["desig_observaciones"], categoria=categoria)
+    session.commit()        
 
-    
     extensiones = obtener_extensiones(id, cur)
     for e in extensiones:
         if e["extension_prorrogada_con"]:
@@ -90,8 +114,9 @@ def importar_categorias(cur, s):
             categoria.id = str(uuid.uuid4())
             categoria.old_id = str(c["tipobajadesig_id"])
             categoria.nombre = "Baja por {}".format(c["tipobajadesig_nombre"])
-        logging.info(categoria.__dict__)
-    #session.commit()
+            session.add(categoria)
+            logging.info(categoria.__dict__)
+    session.commit()
 
 if __name__ == '__main__':
     conn = psycopg2.connect("host='{}' user='{}' password='{}' dbname={}".format(
@@ -106,12 +131,10 @@ if __name__ == '__main__':
             importar_categorias(cur, session)
             
             cur.execute(""" SELECT * 
-            FROM designacion_docente where desig_id =  208
+            FROM designacion_docente where desig_id =  108
             """)
-            """
             for d in cur:
-                importar_designacion(d, cur)
-            """
+                importar_designacion(d, cur, session)
 
     finally:
         cur.close()
