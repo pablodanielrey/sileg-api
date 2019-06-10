4f09c410ad98cbbadd6a33ff7719e27faa62d745
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-06-10 17:33:02 -0300

    prueba para registrar los permisos de los sistemas

--- src/sileg/api/rest/gelis.py
+++ src/sileg/api/rest/gelis.py
@@ -4,14 +4,28 @@ from .converters import ListConverter
 
 bp = Blueprint('gelis', __name__, url_prefix='/gelis/api/v1.0')
 
+permisos = {
+    'DESIGNATIONS_READ':'urn:gelis:designations:read',
+    'PLACE_CREATE':'urn:gelis:place:create'
+}
+
+def obtener_lista_permisos(permisos):
+    return [p for p in permisos.values()]
+
+
 @bp.before_app_request
 def antes_de_cada_requerimiento():
     pass
 
-@bp.route('/designaciones', methods=['GET'])
-def designaciones_por_lugares():
-    lids = request.args.get('lids',None)
-    if not lids:
-        return (400,'Invalid')
+import os
+WARDEN_API_URL = os.environ['WARDEN_API_URL']
 
-    return []
+@bp.route('/registrar', methods=['GET'])
+def designaciones_por_lugares():
+    import requests
+    data = {
+        'system': 'gelis-api',
+        'permissions': obtener_lista_permisos(permisos)
+    }
+    r = requests.post(WARDEN_API_URL + '/registrar_permisos', json=data)
+    return r
