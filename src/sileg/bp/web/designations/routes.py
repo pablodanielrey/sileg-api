15e2f9935c01b93c03a3d9856675b5b9b66ba6ea
Leonardo Consolini <leonardo.consolini@gmail.com>
2019-11-25 14:44:33 -0300

    se crea pantalla para crear designacion. Fixes #14

--- src/sileg/bp/web/designations/routes.py
+++ src/sileg/bp/web/designations/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template, flash, redirect,request, Markup, url_for
 
+from .forms import DesignationCreateForm
+
 from . import bp
 
 @bp.route('/crear')
@@ -7,7 +9,13 @@ def create():
     """
     Pagina de creacion de designacion
     """
-    return render_template('createDesignations.html')
+    form = DesignationCreateForm()
+    user = {
+        'dni': '12345678',
+        'firstname': 'Pablo',
+        'lastname': 'Rey'
+    }
+    return render_template('createDesignations.html', user=user, form=form)
 
 @bp.route('/buscar')
 def search():
