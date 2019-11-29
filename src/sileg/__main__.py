c95665e34f3c6b8b017d54b0df7afe082310d986
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-11-29 14:25:46 -0300

    cambiamos para que tenga el puerto de ui

--- src/sileg/__main__.py
+++ src/sileg/__main__.py
@@ -2,7 +2,7 @@
 from sileg import app
 
 def main():
-    app.run(host='0.0.0.0', port=10202, debug=False)
+    app.run(host='0.0.0.0', port=10203, debug=False)
 
 if __name__ == '__main__':
     main()
