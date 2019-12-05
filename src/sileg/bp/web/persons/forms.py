31ebe61691b30bdc4de81e2d1528cbce3da15d79
Miguel Angel Macagno <miguel.macagno@gmail.com>
2019-12-04 21:24:17 -0300

    Correccion de formulario de creacion de persona

--- src/sileg/bp/web/persons/forms.py
+++ src/sileg/bp/web/persons/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, DateTimeField, SelectField
+from wtforms import StringField, PasswordField, BooleanField, DateTimeField, SelectField, FileField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import ValidationError, DataRequired, EqualTo, Email
 
@@ -11,55 +11,52 @@ class PersonCreateForm(FlaskForm):
     person_number_type = SelectField('Tipo de Documento', coerce=str)
     person_number = StringField('Nro. de Documento', validators=[DataRequired()])
     gender = SelectField('Genero', coerce=str)
-    birthplace = StringField('Ciudad de Nacimiento',validators=[DataRequired()])
-    birthdate = DateTimeField('Fecha de Nacimiento',validators=[DataRequired()],format='%d-%m-%Y')
-    
-    residence = StringField('Ciudad de Residencia',validators=[DataRequired()])
-    address = StringField('Dirección', validators=[DataRequired()])
-    
-    work_email = EmailField('Correo de Trabajo', validators=[DataRequired(), Email(message='Formato de correo erroneo.')])
-    personal_email = EmailField('Correo Personal', validators=[DataRequired(), Email(message='Formato de correo erroneo.')])
-    
-    land_line = StringField('Telefono Fijo', validators=[])
-    mobile_number =StringField('Telefono Movil', validators=[])
-
-    ingress_date = DateTimeField('Fecha de ingreso a la FCE')
-    laboral_number = StringField('CUIL')
     marital_status = SelectField('Estado Civil', coerce=str)
-    retirement_date = DateTimeField('Fecha de Jubilación')
-    
-    #seniority_to_date = DateTimeField('Antigüedad al Día')
-    #seniority_rented_internal = DateTimeField('Antigüedad Rentada Interna')
-    #seniority_rented_external = DateTimeField('Antigüedad Rentada Externa')
-    #seniority_rented_total = DateTimeField('Antigüedad Rentada Total')
-    #seniority_rented_unused = DateTimeField('Antigüedad Rentada No Utilizada')
-    #seniority_ad_honorem = DateTimeField('Antigüedad Ad-Honorem')
-    #seniority_rented_exported = DateTimeField('Antigüedad Rentada Exportada')
-    #seniority_ad_honorem = BooleanField('¿Antigüedad Ad-Honorem Computable?	')
-    #seniority_observations = StringField('Observaciones')
-
-    #prepaid_medicine = SelectField('Obra Social', coerce=str)
-    #life_insurance = SelectField('Seguro de Vida', coerce=str)
-    #life_insurance_account = StringField('Cuenta Nro.')
-    #bank_branch = SelectField('Cobra en Banco/Sucursal', coerce=str)
-    #life_insurance_mandatary = BooleanField('Obligatorio')
-    #life_insurance_bank = SelectField('Banco', coerce=str)
-    #life_insurance_policy_number1 = StringField('Poliza Número 1')
-    #life_insurance_policy_number2 = StringField('Poliza Número 2')
-    #life_insurance_policy_number3 = StringField('Poliza Número 3')
-       
+    birthplace = StringField('Ciudad de Nacimiento')
+    birthdate = StringField('Fecha de Nacimiento')
+    residence = StringField('Ciudad de Residencia')
+    address = StringField('Dirección')
+    work_email = EmailField('Correo de Trabajo')
+    personal_email = EmailField('Correo Personal')
+    land_line = StringField('Telefono Fijo')
+    mobile_number =StringField('Telefono Movil')
+    person_numberFile = FileField('Adjuntar DNI')
+    laboral_numberFile = FileField('Adjuntar CUIL')
+    seniority_external_years = StringField('Años')
+    seniority_external_months = StringField('Meses')
+    seniority_external_days = StringField('Días')
         
     def __init__(self):
         super(PersonCreateForm,self).__init__()
-        self.person_number_type.choices = [('0','Seleccione una opción...'),('0','LC'),('0','LE'),('0','DNI'),('0','PASAPORTE')]
-        self.gender.choices = [('0','Seleccione una opción...'),('0','Femenino'),('0','Masculino'),('0','Otro')]
-        self.marital_status.choices = [('0','Seleccione una opción...'),('0','Casado/a'),('0','Soltero/a'),('0','Conviviente'),('0','Divorciado/a'),]
+        self.person_number_type.choices = [('0','Seleccione una opción...'),('1','LC'),('2','LE'),('3','DNI'),('4','PASAPORTE')]
+        self.gender.choices = [('0','Seleccione una opción...'),('1','Femenino'),('2','Masculino'),('3','Autopercibido')]
+        self.marital_status.choices = [('0','Seleccione una opción...'),('1','Casado/a'),('2','Soltero/a'),('3','Conviviente'),('4','Divorciado/a'),]
 
-    def validate_person_number(self, person_number):
-        #if UsersModel.getUser(person_number.data):
-        #    raise ValidationError('Atencion existente un usuario con ese DNI')
-        pass
+    def validate_person_number_type(self, person_number_type):
+        if self.person_number_type.data == '0':
+            raise ValidationError('Debe seleccionar una opción')
+    
+    def validate_lastname(self,lastname):
+        if self.lastname.data == 'algo':
+            raise ValidationError('Debe seleccionar una opción')
+class TitleAssignForm(FlaskForm):
+    titleType = SelectField('Tipo de Título')
+    titleDate = StringField('Fecha de Obtención')
+    titleName = SelectField('Nombre del Título')
+    titleFile = FileField('Adjuntar Título')
+        
+    def __init__(self):
+        super(TitleAssignForm,self).__init__()
+        #TODO Obtener del modelo los titulos existentes y tipos
+        self.titleType.choices = [('0','Seleccione una opción...'),('1','Grado'),('2','Posgrado')]
+        self.titleName.choices = [('0','Seleccione una opción...'),('1','Contador'),('2','Ingeniero'),('3','Licenciado en Ciencias Administrativas')]
 
+    def validate_titleType(self, titleType):
+        if self.titleType.data == '0':
+            raise ValidationError('Debe seleccionar una opción')
+    def validate_titleName(self, titleName):
+        if self.titleName.data == '0':
+            raise ValidationError('Debe seleccionar una opción')
 
 class PersonSearchForm(FlaskForm):
     query = StringField('Buscar persona por apellido o número de documento')
