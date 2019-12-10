584bebff44b4115bade7b8982b318c251f1d5b32
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-12-10 17:18:41 -0300

    cree una clase para manejar el problema del chequeo de azp del token

--- src/sileg/auth.py
+++ src/sileg/auth.py
@@ -1,4 +1,25 @@
 
 from sileg import app
 from flask_oidc import OpenIDConnect
-oidc = OpenIDConnect(app)
+
+class MyOpenIDConnect(OpenIDConnect):
+    """
+        clase necesaria para corregir la mala implementación del chequeo del tokenid.
+        ahora para resolver el caso retorno True
+    """
+    def _is_id_token_valid(self, id_token):
+        """
+        Check if `id_token` is a current ID token for this application,
+        was issued by the Apps domain we expected,
+        and that the email address has been verified.
+
+        @see: http://openid.net/specs/openid-connect-core-1_0.html#IDTokenValidation
+        """
+        if not id_token:
+            return False
+
+        return True    
+
+
+
+oidc = MyOpenIDConnect(app)
