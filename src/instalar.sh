e78761f7b891bfb7d7e4888dad35a226588aac8a
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-12-12 15:56:25 -0300

    agrego el script para instalar correcto

--- src/instalar.sh
+++ src/instalar.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-pip3 install -U --upgrade-strategy=eager --trusted-host pypi.econo.unlp.edu.ar -e .
-#npm install -g @angular/cli
+pip3 install -U --no-cache --upgrade-strategy=eager --trusted-host pypi.econo.unlp.edu.ar --extra-index-url http://pypi.econo.unlp.edu.ar:8080 -e .
