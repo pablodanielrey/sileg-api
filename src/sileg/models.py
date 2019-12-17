67f527ed7dd795c45d4c5486d1af0616d066354c
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-12-16 21:12:04 -0300

    config para obtener el modelo

--- src/sileg/models.py
+++ src/sileg/models.py
@@ -2,5 +2,11 @@
 from users.model.UsersModel import UsersModel
 import users.model
 
+from sileg_model.model.SilegModel import SilegModel
+import sileg_model.model
+
 usersModel = UsersModel
 open_users_session = users.model.open_session
+
+silegModel = SilegModel
+open_sileg_session = sileg_model.model.open_session
