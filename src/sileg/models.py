dd359bef47fafb1b2c68c36fbd1a6675f9f9494d
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-12-27 15:39:35 -0300

    actualizamos la forma de crear las peronas para que tengan el dni como tabla relacionada. tambien los archvios

--- src/sileg/models.py
+++ src/sileg/models.py
@@ -1,13 +1,11 @@
 
 from users.model.UsersModel import UsersModel
-from users.model.entities.User import User, Mail, Phone, UserFiles, MailTypes, PhoneTypes, UserFileTypes
 import users.model
 
 from sileg_model.model.SilegModel import SilegModel
 import sileg_model.model
 
 usersModel = UsersModel
-
 open_users_session = users.model.open_session
 
 silegModel = SilegModel()
