782d8bb5527cc904a20a42a4d1981fdb87ff09d8
Leonardo Consolini <leonardo.consolini@gmail.com>
2020-02-19 13:21:25 -0300

    se pasan los datos al formulario y luego a la vista

--- src/sileg/bp/web/persons/routes.py
+++ src/sileg/bp/web/persons/routes.py
@@ -92,8 +92,37 @@ def modifyPersonData(user,uid):
             abort(404)
         person = persons[0]
         form = PersonModifyForm()
+        if person.lastname:
+            form.lastname.data = person.lastname
+        if person.firstname:
+            form.firstname.data = person.firstname
+        if len(person.identity_numbers) > 0:
+            for pi in person.identity_numbers:
+                if pi.type.value != 'CUIL' and pi.type.value != 'CUIT':
+                    form.person_number_type.data = pi.type.value
+                    form.person_number.data = pi.number
+        if person.birthplace:
+            form.birthplace.data = person.birthplace
+        if person.birthdate:
+            form.birthdate.data = person.birthdate.strftime('%d/%m/%Y')
+        if person.address:
+            form.address.data = person.address
+        if person.residence:
+            form.residence.data = person.residence
+        if len(person.mails) > 0:
+            for pm in person.mails: 
+                if pm.type.value == 'INSTITUTIONAL':
+                    form.work_email.data = pm.email
+                if pm.type.value == 'ALTERNATIVE':
+                    form.personal_email.data = pm.email
+        if len(person.phones) > 0:
+            for ph in person.phones:
+                if ph.type.value == 'LANDLINE':
+                    form.land_line.data = ph.number
+                if ph.type.value == 'CELLPHONE':
+                    form.mobile_number == ph.number
         if form.validate_on_submit():
             form.save(user['sub'])
 
-    return render_template('modifyPerson.html', user=user, person=person, form=form)
+    return render_template('modifyPerson.html', user=user, form=form)
     
