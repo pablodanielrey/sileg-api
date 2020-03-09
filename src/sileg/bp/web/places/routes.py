96c91bab9e9a2db04412935e91fc641213a7e05f
Miguel Angel Macagno <miguel.macagno@gmail.com>
2020-03-09 19:17:32 -0300

    Se agrega pantalla de alta lugar aun sin terminar

--- src/sileg/bp/web/places/routes.py
+++ src/sileg/bp/web/places/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template, flash, redirect,request, Markup, url_for
+from flask import render_template, flash, redirect,request, Markup, url_for, abort
 from sileg.auth import require_user
 
-from .forms import PlaceSearchForm
+from .forms import PlaceSearchForm,PlaceCreateForm
+
+from sileg.models import open_sileg_session
 
 from . import bp
 
@@ -9,17 +11,30 @@ from . import bp
 @require_user
 def create(user):
     """
-    Pagina de creacion de usuarios GET
+    Pagina de creacion de lugares GET
     """
-    return render_template('places.html',user=user)
+    form = PlaceCreateForm()
+    return render_template('createPlace.html',user=user,form=form)
 
 @bp.route('/crear', methods=['POST'])
 @require_user
 def create_post(user):
     """
-    Pagina de creacion de usuarios POST
+    Pagina de creacion de lugares POST
     """
-    return render_template('places.html',user=user)
+    form = PlaceCreateForm()
+    if not form.validate_on_submit():
+        flash(Markup('<span>Error al crear lugar!</span>'))
+        print(form.errors)
+        return render_template('createPlace.html',user=user,form=form)
+    if form.type.data == '0':
+        flash(Markup('<span>Por favor seleccione un tipo de lugar!</span>'))
+        return render_template('createPlace.html',user=user,form=form)
+    with open_sileg_session() as session:     
+        form.save(session, user['sub'])
+        #session.commit()
+        flash(Markup('<span>¡Lugar Creado!</span>'))
+    return redirect(url_for('places.search', query=form.name.data))
 
 @bp.route('/buscar')
 @require_user
