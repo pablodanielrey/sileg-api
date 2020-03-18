0120a3e35d430497dfddfaa3ac6bd1975abb78d1
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-03-18 17:50:22 -0300

    agrego a pablo lozada al modelo de seguridad provisorio

--- src/sileg/helpers/permissionsHelper.py
+++ src/sileg/helpers/permissionsHelper.py
@@ -30,7 +30,8 @@ def verify_students_permission(fn):
             '34770038':'13b2471b-507e-44d7-a440-efdb66d5aaa8',
             '26106655':'4719252a-ef58-4522-a776-d497708d2812',
             '27528150':'853cd3dd-739c-4423-a88e-4fe722209fc7',
-            '27528195':'af2ba1fb-66ce-4857-b226-1b19b1d3a0c5'            
+            '27528195':'af2ba1fb-66ce-4857-b226-1b19b1d3a0c5',
+            '32393755':'15022185-5e14-4772-a620-53fadf843bc0'            
         }
 
         if uid in nuestros_uids.values():
