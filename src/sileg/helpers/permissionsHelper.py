ed7415dd539344c51be9ef29ebb59660a63e29d8
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-03-18 17:49:56 -0300

    agrego a pablo lozada a los admins de sileg.

--- src/sileg/helpers/permissionsHelper.py
+++ src/sileg/helpers/permissionsHelper.py
@@ -50,7 +50,8 @@ def verify_sileg_permission(fn):
             '34928857':'3ca3057b-adba-49b3-8b99-550311fc9c81',
             '34770038':'13b2471b-507e-44d7-a440-efdb66d5aaa8',
             '27528150':'853cd3dd-739c-4423-a88e-4fe722209fc7',
-            '27528195':'af2ba1fb-66ce-4857-b226-1b19b1d3a0c5'
+            '27528195':'af2ba1fb-66ce-4857-b226-1b19b1d3a0c5',
+            '32393755':'15022185-5e14-4772-a620-53fadf843bc0'
         }
 
         if uid in nuestros_uids.values():
