b76c5181745c149a17fb4c2ec6ab3d01cb8e8e39
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-03-25 17:19:49 -0300

    agregue a wally a ivan y a jano

--- src/sileg/helpers/permissionsHelper.py
+++ src/sileg/helpers/permissionsHelper.py
@@ -9,7 +9,10 @@ def verify_admin_permissions(fn):
         nuestros_uids = {
             '27294557':'89d88b81-fbc0-48fa-badb-d32854d3d93a',
             '34928857':'3ca3057b-adba-49b3-8b99-550311fc9c81',
-            '34770038':'13b2471b-507e-44d7-a440-efdb66d5aaa8'
+            '34770038':'13b2471b-507e-44d7-a440-efdb66d5aaa8',
+            '29694757':'35f7a8a6-d844-4d6f-b60b-aab810610809',
+            '31073351':'d44e92c1-d277-4a45-81dc-a72a76f6ef8d',
+            '30001823':'205de802-2a15-4652-8fde-f23c674a1246'            
         }
 
         if uid in nuestros_uids.values():
@@ -31,7 +34,8 @@ def verify_students_permission(fn):
             '26106655':'4719252a-ef58-4522-a776-d497708d2812',
             '27528150':'853cd3dd-739c-4423-a88e-4fe722209fc7',
             '27528195':'af2ba1fb-66ce-4857-b226-1b19b1d3a0c5',
-            '32393755':'15022185-5e14-4772-a620-53fadf843bc0'            
+            '32393755':'15022185-5e14-4772-a620-53fadf843bc0',
+            '30001823':'205de802-2a15-4652-8fde-f23c674a1246'
         }
 
         if uid in nuestros_uids.values():
@@ -55,7 +59,8 @@ def verify_sileg_permission(fn):
             '27528150':'853cd3dd-739c-4423-a88e-4fe722209fc7',
             '27528195':'af2ba1fb-66ce-4857-b226-1b19b1d3a0c5',
             '32393755':'15022185-5e14-4772-a620-53fadf843bc0',
-            '26106655':'4719252a-ef58-4522-a776-d497708d2812'
+            '26106655':'4719252a-ef58-4522-a776-d497708d2812',
+            '30001823':'205de802-2a15-4652-8fde-f23c674a1246'
         }
 
         if uid in nuestros_uids.values():
