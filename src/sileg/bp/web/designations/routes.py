2c44c811f2ab279bddb2ef310d427790627a2376
Miguel Angel Macagno <miguel.macagno@gmail.com>
2020-04-07 17:37:55 -0300

    +Designaciones pendientes

--- src/sileg/bp/web/designations/routes.py
+++ src/sileg/bp/web/designations/routes.py
@@ -773,3 +773,14 @@ def modifyDesignation_post(user, did):
     return redirect(url_for('designations.designation_detail', did=did))
 
 
+@bp.route('/pendientes')
+@require_user
+@verify_admin_permissions
+def pendingDesignations(user):
+    """
+    Listado de designaciones en estado pendiente
+    """
+    with open_sileg_session() as session:
+        pendingIds = silegModel.get_designations_pending(session)
+        pendings = silegModel.get_designations(session,pendingIds)
+        return render_template('pendingDesignations.html', user=user, pendings=pendings, dt2s=dt2s, find_user=_find_user)
