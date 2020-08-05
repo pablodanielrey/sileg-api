e1ed84f175107467a7f63882258ac0aa1e1f59c3
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-08-05 17:44:56 -0300

    anule pulsar para los eventos

--- src/sileg/EventsModel.py
+++ src/sileg/EventsModel.py
@@ -7,26 +7,31 @@ from pulsar.schema import JsonSchema
 from login.model.entities.Login import LoginEvent, LoginEventTypes
 from users.model.entities.User import UserEventTypes, UserEvent
 
-PULSAR_URL = os.environ.get('PULSAR_URL', 'pulsar://localhost:6650')
-PULSAR_TOPIC = os.environ.get('PULSAR_TOPIC', 'google')
-PULSAR_SUBSCRIPTION = os.environ.get('PULSAR_SUBSCRIPTION', 'google')
+#PULSAR_URL = os.environ.get('PULSAR_URL', 'pulsar://localhost:6650')
+#PULSAR_TOPIC = os.environ.get('PULSAR_TOPIC', 'google')
+#PULSAR_SUBSCRIPTION = os.environ.get('PULSAR_SUBSCRIPTION', 'google')
 
 import logging
 
 class EventsModel:
 
     def __init__(self):
+        """
         self.client = pulsar.Client(PULSAR_URL)
         self.producer = self.client.create_producer(topic=PULSAR_TOPIC, schema=JsonSchema(LoginEvent))
         self.user_producer = self.client.create_producer(topic=f"user_{PULSAR_TOPIC}", schema=JsonSchema(UserEvent))
+        """
         self.logging = logging.getLogger(self.__class__.__qualname__)
 
+
+    """
     def __del__(self):
         try:
             self.producer.close()
         except:
             pass
         self.client.close()
+    """
 
     def send(self, username, credentials):
         self.logging.debug(f'enviando el evento de {username} a pulsar')
@@ -34,7 +39,8 @@ class EventsModel:
             msg = LoginEvent(type_=LoginEventTypes.CHANGE_CREDENTIALS.value, 
                             username=username, 
                             credentials=credentials)
-            self.producer.send(msg)
+            #self.producer.send(msg)
+            self.logging.log(msg)
         except Exception as e:
             self.logging.exception(e)
             raise e
@@ -44,7 +50,8 @@ class EventsModel:
             data = json.dumps(user.to_dict())
             event = UserEvent(type_=UserEventTypes.CREATED.value,
                             user=data)
-            self.user_producer.send(event)
+            #self.user_producer.send(event)
+            self.logging.log(event)
         except Exception as e:
             self.logging.exception(e)
             raise e
@@ -54,7 +61,8 @@ class EventsModel:
             data = json.dumps(user.to_dict())
             event = UserEvent(type_=UserEventTypes.UPDATED.value,
                             user=data)
-            self.user_producer.send(event)
+            #self.user_producer.send(event)
+            self.logging.log(event)
         except Exception as e:
             self.logging.exception(e)
             raise e
