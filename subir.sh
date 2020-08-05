a010ba21bf579e9d5726bfe4ac38ba411e788cbd
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-08-05 18:45:47 -0300

    fixes #122

---
+++ subir.sh
@@ -0,0 +1,4 @@
+docker tag sileg-api-testing registry.econo.unlp.edu.ar/sileg-api-testing:v0.3.0
+docker push registry.econo.unlp.edu.ar/sileg-api-testing:v0.3.0
+docker tag sileg-api-testing registry.econo.unlp.edu.ar/sileg-api-testing:latest
+docker push registry.econo.unlp.edu.ar/sileg-api-testing:latest
