8f4b9925b63a113501d6d8c6c8dd94f7e35b6a4a
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-08-14 13:36:52 -0300

    incremento la versión para psarlo a producción

--- subir-imagen.sh
+++ subir-imagen.sh
@@ -1,4 +1,4 @@
-docker tag sileg-api-testing registry.econo.unlp.edu.ar/sileg-api-testing:v0.3.8
-docker push registry.econo.unlp.edu.ar/sileg-api-testing:v0.3.8
+docker tag sileg-api-testing registry.econo.unlp.edu.ar/sileg-api-testing:v0.3.9
+docker push registry.econo.unlp.edu.ar/sileg-api-testing:v0.3.9
 docker tag sileg-api-testing registry.econo.unlp.edu.ar/sileg-api-testing:latest
 docker push registry.econo.unlp.edu.ar/sileg-api-testing:latest
